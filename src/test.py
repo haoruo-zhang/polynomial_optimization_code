Fix multiplier gradient issue
Some lagrange multiplier calculations were still using the
original mu >= 0 constraint for some things

src/test.py
@@ -541,8 +541,8 @@ class TestMultiplierGradient(unittest.TestCase):
                                  self.free_vars.R, L, D,
                                  d)
         self.assertTrue(np.isclose(jax_result, hardcoded_result).all())
-        self.assertTrue(np.isclose(old_result, hardcoded_result).all())
-        self.assertTrue(np.isclose(old_result, jax_result).all())
+        #self.assertTrue(np.isclose(old_result, hardcoded_result).all())
+        #self.assertTrue(np.isclose(old_result, jax_result).all())
 
         # Test if "random" (but fixed) factorization and lagrange multipliers
         # yield the same answer
@@ -558,8 +558,8 @@ class TestMultiplierGradient(unittest.TestCase):
                                  self.lm.relaxation, self.free_vars.mu,
                                  self.free_vars.R, L, D,
                                  d)
-        self.assertTrue(np.isclose(old_result, hardcoded_result).all())
-        self.assertTrue(np.isclose(old_result, jax_result).all())
+        #self.assertTrue(np.isclose(old_result, hardcoded_result).all())
+        #self.assertTrue(np.isclose(old_result, jax_result).all())
         self.assertTrue(np.isclose(jax_result, hardcoded_result).all())
 
     def test_relaxation_mu(self):
@@ -699,11 +699,6 @@ class TestMultiplierGradient(unittest.TestCase):
                                  self.free_vars.R, L, D,
                                  d)
         self.assertTrue(np.isclose(jax_result, hardcoded_result).all())
-
-        # specifically check that no infeasibility detected in these positions,
-        # as any mu >= 0 should be acceptable
-        self.assertTrue(np.equal(jax_result[:,0], np.zeros((L,))).all())
-        self.assertTrue(np.equal(hardcoded_result[:,0], np.zeros((L,))).all())
 
         # Changing mu without updating M_d should break the agreement
         # between the jax gradient (uses mu and M_d) and our hardcoded one,
@@ -1586,7 +1581,7 @@ class TestSolver(unittest.TestCase):
         # TODO is this correct?
         self.d = 8
         poly = ExampleF(self.D)
-        solver(poly, self.gamma, self.L, self.D, self.d)
+        solver(poly, self.gamma)
         return
 
     def paper_example_2(self):

src/utils.py
@@ -290,8 +290,9 @@ def multiply_lagrangian(l_factorization, l_nonnegativity, l_relaxation,
     total += jnp.einsum('abij,abij->', M_d - RRt, l_factorization)
     
     # NOTE changed from paper's >= 0 to == 1 (below)
+    # so this part is now obsolete
     # 5. mu_(1,0)^l>=0, so anything positive is clipped
-    total += jnp.minimum(mu[:,0,0], 0) @ l_nonnegativity[:,0]
+    #total += jnp.minimum(mu[:,0,0], 0) @ l_nonnegativity[:,0]
 
     # NOTE this is a change from the paper. Now we require all zeroth moments
     # in first coordinate to be 1
@@ -457,12 +458,12 @@ def grad_mu(l_factorization, l_nonnegativity, l_relaxation,
         for k in range(number):
             result[:,:,n_i] += l_factorization[:,:,lower+k,upper-1-k]
 
+    # NOTE removed because we're making it == 1 for all i = 1, ..., D
     # highlight infeasible mu_1,0 (in this case, negatives)
     #infeas = np.copy(mu[:,0,0])
     #infeas[infeas >= 0] = 0
     #result[:,0,0] += np.where(infeas < 0, l_nonnegativity[:,0], infeas)
 
-    # NOTE removed because we're making it == 1 for all i = 1, ..., D
     # This averages the "derivative" in both directions because of the
     # nondifferentiability of the infeasibility function at 0
     #result[:,0,0] += 0.5 * np.where(mu[:,0,0] < 0, l_nonnegativity[:,0], np.zeros_like(mu[:,0,0]))